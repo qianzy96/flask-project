3249e3c0dc4b4b94c3264bee115d90c02abf7e00
qianzy96 <22842347@qq.com>
2015-09-23 23:17:25 +0800

    NOT template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = { 'nickname': 'Miguel' } # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
