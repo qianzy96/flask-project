4b8031af79481bda5544e93553b09e7b4c867761
qianzy96 <22842347@qq.com>
2015-09-28 08:59:41 +0800

    login1

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-#print basedir
-class Config:
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
-    @staticmethod
-    def init_app(app):
-        pass
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+#OPENID_PROVIDERS = [
+#    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+#    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+#    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+#    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+#    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
